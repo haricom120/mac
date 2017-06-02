b1b9b0124c89ef88de4064a062a794bbdcc08ff4
haricom120 <haricom.120@gmail.com>
2017-06-02 15:14:26 -0500

    angular inst

---
+++ My3/ANGULAR/you/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the youApp
+ */
+angular.module('youApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ My3/ANGULAR/you/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youApp
+ */
+angular.module('youApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
